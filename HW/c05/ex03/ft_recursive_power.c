d360beb9c62f07a36d13e309d82991b8a174e7d3
Boss2 <kt_boss@hotmail.com>
2024-07-25 22:35:55 +0700

    New

--- HW/c05/ex03/ft_recursive_power.c
+++ HW/c05/ex03/ft_recursive_power.c
@@ -6,25 +6,16 @@ int ft_recursive_power(int nb, int power)
    {
         return 0;
     }
-
-    if (nb == 0 && power == 0) 
+    if (power == 0) 
     {
         return 1;
-    }
-    
-    int result = 1;
-   
-    while (power > 0) 
+    } 
+    if (power >1) 
     {
-        result *= nb;
-        power--;
-    }
-    if (result>=2147483647) 
-    {
-        printf("undefind");
-    }
+        nb = nb * ft_recursive_power(nb, power - 1);         
+    } 
     
-    return result;
+    return nb;
 }
 
 int main() {
